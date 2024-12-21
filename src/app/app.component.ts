23627e3caba63c7468f25df99097e562ea74a606
BrandonEHM <brandonhermart@gmail.com>
2024-12-20 22:04:26 -0600

    Brandon Enrique Hernandez Martinez

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroeListComponent } from "./components/heroe-list/heroe-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeroeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ejercicio17';
+  title = 'cbdf_behm_ejercicio17';
 }
---
+++ src/app/components/heroe-list/heroe-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, inject } from '@angular/core';
+import { HeroeService } from '../../services/heroe-service.service';
+import { Heroe } from '../../data/heroe-entity';
+import { Heroes } from '../../data/heroes-entity';
+
+@Component({
+  selector: 'app-heroe-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroe-list.component.html',
+  styleUrl: './heroe-list.component.css'
+})
+export class HeroeListComponent {
+  listadoHeroes: any[] = [];
+  heroeService = inject(HeroeService);
+
+  constructor() {
+    this.heroeService.getAllHeroes().subscribe((data) => {
+      this.listadoHeroes = data.heroes
+      console.info(data.heroes,
+        // data.data,
+        // data.links,
+        // data.msg
+      )
+
+    })
+  }
+
+  CargarHeroes() {
+    this.heroeService.getAllHeroes().subscribe((data) => {
+      this.listadoHeroes = data.heroes
+    })
+  }
+
+  EliminarHeroe(id: number) {
+    this.heroeService.deleteHeroe(id).subscribe((data) => {
+      if (data.estado == 1) {
+        this.CargarHeroes()
+      } else {
+        alert(data.msg)
+      }
+
+    })
+  }
+
+  CrearHeroe(heroe: any) {
+    this.heroeService.postHeroe(heroe).subscribe((data) => {
+      alert("heroe creado")
+    })
+  }
+  
+  trackById(index: number, heroe: any): number {
+    return heroe.id;
+  }
+  
+}
